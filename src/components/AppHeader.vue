121c7d2f8f2102fbd67a069d34a3d07586eb8126
Limson91 <matteo.a.colautti@gmail.com>
2023-11-17 16:41:23 +0100

    add - header format

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -18,28 +18,56 @@ export default {
 <template>
     <AppHeaderInfo />
 
-    <div class="nav-header">
-        <nav class="nav-container">
-            <div class="nav-row">
-                <div class="nav-logo-col">
-                    <img class="nav-logo" src="nexgen-logo.svg">
+    <div class="hero">
+        <div class="nav-header">
+            <nav class="nav-container">
+                <div class="nav-row">
+                    <div class="nav-logo-col">
+                        <img class="nav-logo" src="nexgen-logo.svg">
+                    </div>
+
+                    <div class="nav-menu-col">
+                        <ul class="nav-menu-list">
+                            <li class="nav-menu-item">HOME</li>
+                            <li class="nav-menu-item">ABOUT</li>
+                            <li class="nav-menu-item">SERVICES</li>
+                            <li class="nav-menu-item">PROCESS</li>
+                            <li class="nav-menu-item">TEAM</li>
+                            <li class="nav-menu-item">BLOG</li>
+
+                            <button class="nav-button">GET IN TOUCH</button>
+                        </ul>
+
+                    </div>
                 </div>
+            </nav>
+        </div>
 
-                <div class="nav-menu-col">
-                    <ul class="nav-menu-list">
-                        <li class="nav-menu-item">HOME</li>
-                        <li class="nav-menu-item">ABOUT</li>
-                        <li class="nav-menu-item">SERVICES</li>
-                        <li class="nav-menu-item">PROCESS</li>
-                        <li class="nav-menu-item">TEAM</li>
-                        <li class="nav-menu-item">BLOG</li>
+        <div class="hero-container">
+            <div class="col-6 hero-card">
+                <h4 class="hero-min-title">FUSIONS & ACQUISITIONS</h4>
+                <h2 class="hero-max-title">INSURANCE<br>CONSULTING</h2>
+                <div class="col-row">
+                    <col-6>
+                        <input class="hero-col-item" type="text" placeholder="Name">
+                    </col-6>
+                    <col-6>
+                        <input class="hero-col-item" type="text" placeholder="Email">
+                    </col-6>
+                    <col-6>
+                        <input class="hero-col-item" type="text" placeholder="Phone">
+                    </col-6>
+                    <col-6>
+                        <input class="hero-col-item" type="text" placeholder="More Info">
+                    </col-6>
 
-                        <button class="nav-button">GET IN TOUCH</button>
-                    </ul>
-
+                </div>
+                <div class="col-row">
+                    <button class="hero-button-solid">GET IN TOUCH</button>
+                    <button class="hero-button">READ MORE</button>
                 </div>
             </div>
-        </nav>
+        </div>
     </div>
 </template>
 
@@ -59,12 +87,61 @@ export default {
     margin: 0 auto;
 }
 
+.hero {
+    height: 812px;
+    background-image: url('header.jpg');
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    display: flex;
+    flex-direction: column;
+}
+
+.hero-container {
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    margin-top: 150px;
+}
+
+.hero-card {
+    width: 512px;
+}
+
+.col-row {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 15px;
+    margin-bottom: 15px;
+}
+
 .nav-row {
     display: flex;
     flex-wrap: wrap;
     height: 50px;
     align-items: center;
     justify-content: space-between;
+}
+
+.hero-min-title {
+    font-size: 14px;
+    font-weight: 500;
+    color: $dark-green;
+}
+
+.hero-max-title {
+    font-size: 65px;
+}
+
+.hero-col-item {
+    background-color: white;
+    border: none;
+    color: darkgray;
+    height: 50px;
+    width: 248px;
+    font-size: 16px;
+    padding: 0 15px;
+    border-radius: 5px;
 }
 
 .nav-menu-list {
@@ -91,7 +168,35 @@ export default {
     margin-left: 20px;
 }
 
+.hero-button-solid {
+    background-color: $dark-green;
+    border: none;
+    color: white;
+    height: 46px;
+    width: 171px;
+    font-size: 16px;
+    text-align: center;
+    text-decoration: none;
+    border-radius: 5px;
+}
+
+.hero-button {
+    background-color: white;
+    border: 1px solid $dark-green;
+    color: $dark-green;
+    height: 46px;
+    width: 171px;
+    font-size: 16px;
+    text-align: center;
+    text-decoration: none;
+    border-radius: 5px;
+}
+
 .nav-logo {
     width: 100px;
+}
+
+.col-6 {
+    flex-basis: calc((100% / 12) * 6);
 }
 </style>
